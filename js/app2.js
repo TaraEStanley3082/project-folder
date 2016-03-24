eb05f05f7e068e5af273b1fc28603a90048c8b65
TaraEStanley3082 <tarastanley3082@gmail.com>
2016-03-24 03:30:18 -0400

    Almost there!
    Added Angularjs goodies, now time to link everything up and add a bit of
    style!

---
+++ js/app2.js
@@ -0,0 +1,11 @@
+var app = angular.module('app', ['ngMessages']);
+
+app.controller('MainCtrl', function MainCtrl() {
+    'use scrict';
+    var vm = this;
+    vm.angularVersion = angular.version.full;
+    
+    
+});
+
+// this bit of angularjs goes with the newsletter request //
